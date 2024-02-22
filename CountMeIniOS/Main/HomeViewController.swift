08cf869629980f900e07409ed52dc487861811d8
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2024-02-22 17:15:43 +0530

    rename to

--- CountMeIniOS/Main/HomeViewController.swift
+++ CountMeIniOS/Main/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CountMeIniOS
 //
 //  Created by Karthik K Manoj on 22/02/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
